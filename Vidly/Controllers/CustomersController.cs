896bdb12cc8dba9e8fe76326ddce0631e79dcae5
Prafulla Tandel <prafullatandel@gmail.com>
2017-04-21 14:08:36 +1000

    Section 3 - End - Before Exercises

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,17 +10,29 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var Customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var Customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (Customer == null)
                 return HttpNotFound();
