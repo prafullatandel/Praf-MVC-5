dbb15ab9f2f2ff231fb450e2f3c31ac2fea9d568
Prafulla <prafullatandel@gmail.com>
2017-05-03 23:55:40 +1000

    Section 4 Exercise Implementing Movies New and Edit

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -12,14 +12,19 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+        
         public Genre Genre { get; set; }
+
+        [Display(Name="Genre")]
+        [Required]
         public byte GenreId { get; set; }
         [Required]
         public DateTime DateAdded { get; set; }
         [Required]
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
         [Required]
+        [Display(Name="Number in Stock")]
         public byte NumberInStock { get; set; }
     }
 
